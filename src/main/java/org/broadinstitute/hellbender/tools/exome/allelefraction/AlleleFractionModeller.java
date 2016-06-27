zyangpointer/gatk-protected

Allele Fraction HMM

src/main/java/org/broadinstitute/hellbender/tools/exome/allelefraction/AlleleFractionModeller.java
@@ -44,6 +44,7 @@ import java.util.stream.IntStream;
  */
 public final class AlleleFractionModeller {
     private final SegmentedModel segmentedModel;
+    final AllelicPanelOfNormals allelicPON;
     private final ParameterizedModel<AlleleFractionState, AlleleFractionData> model;
     private final List<Double> meanBiasSamples = new ArrayList<>();
     private final List<Double> biasVarianceSamples = new ArrayList<>();
@@ -57,6 +58,7 @@ public final class AlleleFractionModeller {
 
     public AlleleFractionModeller(final SegmentedModel segmentedModel, final AllelicPanelOfNormals allelicPON) {
         this.segmentedModel = segmentedModel;
+        this.allelicPON = allelicPON;
         final AlleleFractionData data = new AlleleFractionData(segmentedModel, allelicPON);
         numSegments = data.numSegments();
         final AlleleFractionState initialState = new AlleleFractionInitializer(data).getInitializedState();
@@ -64,14 +66,17 @@ public final class AlleleFractionModeller {
         // Initialization got us to the mode of the likelihood
         // if we approximate conditionals as normal we can guess the width from the curvature at the mode
 
+        final AllelicBiasParameters initialParameters = initialState.getParameters();
+        final AlleleFractionState.MinorFractions initialMinorFractions = initialState.minorFractions();
+
         final double meanBiasInitialStepSize = estimateWidthAtMode(meanBias ->
-                AlleleFractionLikelihoods.logLikelihood(initialState.shallowCopyWithProposedMeanBias(meanBias), data), initialState.meanBias());
+                AlleleFractionLikelihoods.logLikelihood(initialParameters.copyWithNewMeanBias(meanBias), initialMinorFractions, data), initialParameters.getMeanBias());
         final double biasVarianceInitialStepSize = estimateWidthAtMode(biasVariance ->
-                AlleleFractionLikelihoods.logLikelihood(initialState.shallowCopyWithProposedBiasVariance(biasVariance), data), initialState.biasVariance());
+                AlleleFractionLikelihoods.logLikelihood(initialParameters.copyWithNewBiasVariance(biasVariance), initialMinorFractions, data), initialParameters.getBiasVariance());
         final double outlierProbabilityInitialStepSize = estimateWidthAtMode(outlierProbability ->
-                AlleleFractionLikelihoods.logLikelihood(initialState.shallowCopyWithProposedMeanBias(outlierProbability), data), initialState.outlierProbability());
+                AlleleFractionLikelihoods.logLikelihood(initialParameters.copyWithNewOutlierProbability(outlierProbability), initialMinorFractions, data), initialParameters.getOutlierProbability());
         final List<Double> minorFractionsInitialStepSizes = IntStream.range(0, numSegments).mapToDouble(segment ->
-                estimateWidthAtMode(AlleleFractionLikelihoods.segmentLogLikelihoodConditionalOnMinorFraction(initialState, data, segment), initialState.minorFractionInSegment(segment)))
+                estimateWidthAtMode(f -> AlleleFractionLikelihoods.segmentLogLikelihood(initialParameters, f, data.countsInSegment(segment), allelicPON), initialMinorFractions.get(segment)))
                 .boxed().collect(Collectors.toList());
 
         final Sampler<Double, AlleleFractionState, AlleleFractionData> meanBiasSampler =

src/main/java/org/broadinstitute/hellbender/tools/exome/allelefraction/PerformAllelicSegmentation.java
@@ -1,0 +1,83 @@
+package org.broadinstitute.hellbender.tools.exome.allelefraction;
+
+import org.broadinstitute.hellbender.cmdline.Argument;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgram;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgramProperties;
+import org.broadinstitute.hellbender.cmdline.ExomeStandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.programgroups.CopyNumberProgramGroup;
+import org.broadinstitute.hellbender.tools.exome.AllelicCountCollection;
+import org.broadinstitute.hellbender.tools.exome.ModeledSegment;
+import org.broadinstitute.hellbender.tools.exome.SegmentUtils;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by davidben on 5/23/16.
+ */
+@CommandLineProgramProperties(
+        summary = "Segment genomic data into regions of constant minor allele fraction.  Only supports one sample input.",
+        oneLineSummary = "Segment genomic data into regions of constant minor allele fraction",
+        programGroup = CopyNumberProgramGroup.class
+)
+public class PerformAllelicSegmentation extends CommandLineProgram {
+    protected static final String NUM_STATES_LONG_NAME = "numberOfStates";
+    protected static final String NUM_STATES_SHORT_NAME = "numStates";
+
+    protected static final String SAMPLE_LONG_NAME = "sample";
+    protected static final String SAMPLE_SHORT_NAME = "sample";
+
+
+    @Argument(
+            doc = "Input file for tumor-sample ref/alt read counts at normal-sample heterozygous-SNP sites (output of GetHetCoverage tool).",
+            fullName = ExomeStandardArgumentDefinitions.TUMOR_ALLELIC_COUNTS_FILE_LONG_NAME,
+            shortName = ExomeStandardArgumentDefinitions.TUMOR_ALLELIC_COUNTS_FILE_SHORT_NAME,
+            optional = false
+    )
+    protected File snpCountsFile;
+
+    @Argument(
+            doc = "Initial number of hidden allele fraction states",
+            fullName = NUM_STATES_LONG_NAME,
+            shortName = NUM_STATES_SHORT_NAME,
+            optional = false
+    )
+    protected int initialNumStates;
+
+    @Argument(
+            doc = "Input file for allelic-bias panel of normals.",
+            fullName = ExomeStandardArgumentDefinitions.ALLELIC_PON_FILE_LONG_NAME,
+            shortName = ExomeStandardArgumentDefinitions.ALLELIC_PON_FILE_SHORT_NAME,
+            optional = true
+    )
+    protected File allelicPoNFile;
+
+    @Argument(
+            doc = "Output file for allele fraction segments.",
+            fullName = ExomeStandardArgumentDefinitions.SEGMENT_FILE_LONG_NAME,
+            shortName = ExomeStandardArgumentDefinitions.SEGMENT_FILE_SHORT_NAME,
+            optional = false
+    )
+    protected File outputSegmentsFile;
+
+    @Argument(
+            doc = "Sample name.",
+            fullName = SAMPLE_LONG_NAME,
+            shortName = SAMPLE_SHORT_NAME,
+            optional = false
+    )
+    protected String sampleName;
+
+    @Override
+    public Object doWork() {
+        final AllelicPanelOfNormals allelicPoN =
+                allelicPoNFile != null ? new AllelicPanelOfNormals(allelicPoNFile) : AllelicPanelOfNormals.EMPTY_PON;
+        final AllelicCountCollection acc = new AllelicCountCollection(snpCountsFile);
+        final AlleleFractionSegmenter segmenter = new AlleleFractionSegmenter(initialNumStates, acc, allelicPoN);
+        segmenter.learnModel();
+        final List<ModeledSegment> segments = segmenter.findSegments();
+        SegmentUtils.writeModeledSegmentFile(outputSegmentsFile, segments, sampleName, true);
+
+        return "SUCCESS";
+    }
+}

yunzhengluoyi/gatk-protected-1

src/main/java/org/broadinstitute/hellbender/tools/exome/allelefraction/AlleleFractionModeller.java
@@ -42,6 +42,7 @@ import java.util.stream.IntStream;
  */
 public final class AlleleFractionModeller {
     private final SegmentedGenome segmentedGenome;
+    private final AllelicPanelOfNormals allelicPON;
     private final ParameterizedModel<AlleleFractionParameter, AlleleFractionState, AlleleFractionData> model;
     private final List<Double> meanBiasSamples = new ArrayList<>();
     private final List<Double> biasVarianceSamples = new ArrayList<>();
@@ -49,23 +50,35 @@ public final class AlleleFractionModeller {
     private final List<AlleleFractionState.MinorFractions> minorFractionsSamples = new ArrayList<>();
     private final int numSegments;
 
+    public AlleleFractionModeller(final SegmentedGenome segmentedGenome) {
+        this(segmentedGenome, AllelicPanelOfNormals.EMPTY_PON);
+    }
+
     public AlleleFractionModeller(final SegmentedGenome segmentedGenome, final AllelicPanelOfNormals allelicPON) {
         this.segmentedGenome = segmentedGenome;
+        this.allelicPON = allelicPON;
         final AlleleFractionData data = new AlleleFractionData(segmentedGenome, allelicPON);
         numSegments = data.getNumSegments();
         final AlleleFractionState initialState = new AlleleFractionInitializer(data).getInitializedState();
 
         // Initialization got us to the mode of the likelihood
         // if we approximate conditionals as normal we can guess the width from the curvature at the mode and use as the step size
+        final AllelicBiasParameters initialParameters = initialState.getParameters();
+        final AlleleFractionState.MinorFractions initialMinorFractions = initialState.minorFractions();
 
         final double meanBiasInitialStepSize = getStepSizeFromApproximatePosteriorWidthAtMode(meanBias ->
-                AlleleFractionLikelihoods.logLikelihood(initialState.shallowCopyWithProposedMeanBias(meanBias), data), initialState.meanBias());
+                AlleleFractionLikelihoods.logLikelihood(initialParameters.copyWithNewMeanBias(meanBias), initialMinorFractions, data), initialParameters.getMeanBias());
         final double biasVarianceInitialStepSize = getStepSizeFromApproximatePosteriorWidthAtMode(biasVariance ->
-                AlleleFractionLikelihoods.logLikelihood(initialState.shallowCopyWithProposedBiasVariance(biasVariance), data), initialState.biasVariance());
+                AlleleFractionLikelihoods.logLikelihood(initialParameters.copyWithNewBiasVariance(biasVariance), initialMinorFractions, data), initialParameters.getBiasVariance());
         final double outlierProbabilityInitialStepSize = getStepSizeFromApproximatePosteriorWidthAtMode(outlierProbability ->
-                AlleleFractionLikelihoods.logLikelihood(initialState.shallowCopyWithProposedMeanBias(outlierProbability), data), initialState.outlierProbability());
-        final List<Double> minorFractionsInitialStepSizes = IntStream.range(0, numSegments).mapToDouble(segment ->
-                getStepSizeFromApproximatePosteriorWidthAtMode(AlleleFractionLikelihoods.segmentLogLikelihoodConditionalOnMinorFraction(initialState, data, segment), initialState.segmentMinorFraction(segment)))
+                AlleleFractionLikelihoods.logLikelihood(initialParameters.copyWithNewOutlierProbability(outlierProbability), initialMinorFractions, data), initialParameters.getOutlierProbability());
+        
+
+/* Sam
+           getStepSizeFromApproximatePosteriorWidthAtMode(AlleleFractionLikelihoods.segmentLogLikelihoodConditionalOnMinorFraction(initialState, data, segment), initialState.segmentMinorFraction(segment)))
+*/
+final List<Double> minorFractionsInitialStepSizes = IntStream.range(0, numSegments).mapToDouble(segment ->
+        getStepSizeFromApproximatePosteriorWidthAtMode(f -> AlleleFractionLikelihoods.segmentLogLikelihood(initialParameters, f, data.getCountsInSegment(segment), allelicPON), initialMinorFractions.get(segment)))
                 .boxed().collect(Collectors.toList());
 
         final ParameterSampler<Double, AlleleFractionParameter, AlleleFractionState, AlleleFractionData> meanBiasSampler =

src/main/java/org/broadinstitute/hellbender/tools/exome/segmentation/PerformAllelicSegmentation.java
@@ -1,0 +1,85 @@
+package org.broadinstitute.hellbender.tools.exome.segmentation;
+
+import org.broadinstitute.hellbender.cmdline.Argument;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgram;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgramProperties;
+import org.broadinstitute.hellbender.cmdline.ExomeStandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.programgroups.CopyNumberProgramGroup;
+import org.broadinstitute.hellbender.tools.exome.AllelicCountCollection;
+import org.broadinstitute.hellbender.tools.exome.ModeledSegment;
+import org.broadinstitute.hellbender.tools.exome.ReadCountCollectionUtils;
+import org.broadinstitute.hellbender.tools.exome.SegmentUtils;
+import org.broadinstitute.hellbender.tools.exome.allelefraction.AllelicPanelOfNormals;
+import org.broadinstitute.hellbender.tools.exome.segmentation.AlleleFractionSegmenter;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by davidben on 5/23/16.
+ */
+@CommandLineProgramProperties(
+        summary = "Segment genomic data into regions of constant minor allele fraction.  Only supports one sample input.",
+        oneLineSummary = "Segment genomic data into regions of constant minor allele fraction",
+        programGroup = CopyNumberProgramGroup.class
+)
+public class PerformAllelicSegmentation extends CommandLineProgram {
+    protected static final String NUM_STATES_LONG_NAME = "numberOfStates";
+    protected static final String NUM_STATES_SHORT_NAME = "numStates";
+
+    protected static final String SAMPLE_LONG_NAME = "sample";
+    protected static final String SAMPLE_SHORT_NAME = "sample";
+
+
+    @Argument(
+            doc = "Input file for tumor-sample ref/alt read counts at normal-sample heterozygous-SNP sites (output of GetHetCoverage tool).",
+            fullName = ExomeStandardArgumentDefinitions.TUMOR_ALLELIC_COUNTS_FILE_LONG_NAME,
+            shortName = ExomeStandardArgumentDefinitions.TUMOR_ALLELIC_COUNTS_FILE_SHORT_NAME,
+            optional = false
+    )
+    protected File snpCountsFile;
+
+    @Argument(
+            doc = "Initial number of hidden allele fraction states",
+            fullName = NUM_STATES_LONG_NAME,
+            shortName = NUM_STATES_SHORT_NAME,
+            optional = false
+    )
+    protected int initialNumStates;
+
+    @Argument(
+            doc = "Input file for allelic-bias panel of normals.",
+            fullName = ExomeStandardArgumentDefinitions.ALLELIC_PON_FILE_LONG_NAME,
+            shortName = ExomeStandardArgumentDefinitions.ALLELIC_PON_FILE_SHORT_NAME,
+            optional = true
+    )
+    protected File allelicPoNFile;
+
+    @Argument(
+            doc = "Output file for allele fraction segments.",
+            fullName = ExomeStandardArgumentDefinitions.SEGMENT_FILE_LONG_NAME,
+            shortName = ExomeStandardArgumentDefinitions.SEGMENT_FILE_SHORT_NAME,
+            optional = false
+    )
+    protected File outputSegmentsFile;
+
+    @Argument(
+            doc = "Sample name.",
+            fullName = SAMPLE_LONG_NAME,
+            shortName = SAMPLE_SHORT_NAME,
+            optional = false
+    )
+    protected String sampleName;
+
+    @Override
+    public Object doWork() {
+        final AllelicPanelOfNormals allelicPoN =
+                allelicPoNFile != null ? new AllelicPanelOfNormals(allelicPoNFile) : AllelicPanelOfNormals.EMPTY_PON;
+        final AllelicCountCollection acc = new AllelicCountCollection(snpCountsFile);
+        final AlleleFractionSegmenter segmenter = new AlleleFractionSegmenter(initialNumStates, acc, allelicPoN);
+        final List<ModeledSegment> segments = segmenter.findSegments();
+        SegmentUtils.writeModeledSegmentFile(outputSegmentsFile, segments, sampleName, true);
+
+        return "SUCCESS";
+    }
+}